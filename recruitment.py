08ec68a3d2800b11cc854c59019a8be0f340cd52
alshatti92 <a.alshatti@hotmail.co.uk>
2022-09-27 20:29:34 +0300

    not completed

--- recruitment.py
+++ recruitment.py
@@ -1,15 +1,23 @@
 # This function returns a list of skills.
 # This is the list that the user will choose from
 # Add at least 3 random skills for the user to select from
+from platform import python_branch
+from tkinter import N
+
+
 def get_skills():
-    ...
+   skills =  ["python" , "c++" , "Javascript" , "Juggling" , "Running" , "Eating"]
+   return skills
+
+
 
 
 # This function pretty prints the skills to the user
 # It takes the list of skills as an argument and prints them numbered
 # This function doesn't return anything
 def show_skills(skills):
-    ...
+    for skill in skills:
+     print (skill)
 
 
 # Shows the available skills and have user pick from them two skills
@@ -17,13 +25,26 @@ def show_skills(skills):
 # For example, if the user enters 1, the first skill in your list of skills will be added to the list
 # Return a list of the two skills that the user inputted
 def get_user_skills(skills):
-    ...
+    print (show_skills(skills))
+    user_skills1 = input ("choose a skill: ")
+    print (show_skills(skills))
+    user_skills2 = input ("please choose another skill: ")
+    user_skills = ["user_skills1" , "user_skills2"]
+    return user_skills
+
+
 
 
 # This function will get the user's cv from their inputs
 # HINT: Use previous built functions to get the skills from the user
-def get_user_cv(skills):
-    ...
+ def get_user_cv(skills): 
+    person =  {
+        "name": input ("whats your name?  "),
+    "age": input (" how old are you? "),
+    "experience": input ("how many years of experience do you have? "),
+    "skills": "get_user_skills(skills)"
+    }
+    print (person)
 
 
 # This functions checks if the cv is acceptable or not, by checking the age, experience and skills and return a boolean (True or False) based on that
